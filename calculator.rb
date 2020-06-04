a4808859b9e8592879bde00a64ca23f793bcbdc8
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-04 16:23:58 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number = 1.2
-second_number = 2
+first_number = 13
+second_number = 13
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
